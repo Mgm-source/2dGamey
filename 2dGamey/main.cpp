3de0f3dbc2a030274c062175c64365eb30b4756d
Munashe Matadi <Munashe.m@outlook.com>
2025-09-07 07:50:30 +0100

    moved from bigpapa

---
+++ 2dGamey/main.cpp
@@ -0,0 +1,16 @@
+#include "GameWindow.h"
+
+ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR lpCmdLine, int nCmdShow)
+{
+	GameWindow game;
+
+	if (game.init())
+	{
+		while (game.isRunning())
+		{
+			game.run();
+		}
+	}
+	return 0;
+}
